a133e7927769299aa214b7dfd9e8c496a078edb5
davidadeola <davidolorunsesan15@gmail.com>
2023-09-25 18:03:21 +0100

    fix - error generating nonce

--- src/App.js
+++ src/App.js
@@ -1,38 +1,42 @@
 import { useLottie } from "./helpers/useLottie";
-import animationData from "./components/interface/animations/login.json";
-import anotherAnimation from "./components/interface/animations/google.json";
+import { useNonce } from "./helpers/useNonce";
+import loginAnimationData from "./components/interface/animations/login.json";
+import googleAnimationData from "./components/interface/animations/google.json";
 
 function App() {
-  const { container } = useLottie(animationData, true);
-  const { container: newAnimation } = useLottie(anotherAnimation, true);
+  const { container } = useLottie(loginAnimationData, true);
+  const { container: googleAnimationContainer } = useLottie(
+    googleAnimationData,
+    true
+  );
+  const { nonce } = useNonce();
+
   const REDIRECT_URI = "http://localhost:3000/dashboard";
 
   const params = new URLSearchParams({
-    // When using the provided test client ID + redirect site, the redirect_uri needs to be provided in the state.
     state: new URLSearchParams({
       redirect_uri: REDIRECT_URI,
     }).toString(),
-    // Test Client ID for devnet / testnet:
     client_id:
       "25769832374-famecqrhe2gkebt5fvqms2263046lj96.apps.googleusercontent.com",
     redirect_uri: "https://zklogin-dev-redirect.vercel.app/api/auth",
     response_type: "id_token",
     scope: "openid",
-    // See below for details about generation of the nonce
-    nonce: "$NONCE",
+    nonce: nonce,
   });
 
   const loginURL = `https://accounts.google.com/o/oauth2/v2/auth?${params}`;
+
   return (
     <div className="flex flex-col items-center gap-4">
       <h1 className="text-2xl text-gray-600 font-bold">ZKLogin Demo</h1>
       <div ref={container}></div>
       <a
-        className="flex text-lg items-center justify-center w-full gap-4 pr-4 rounded-md text-gray-700 hover:bg-gray-200 max-w-[20em] font-bold bg-[#e4e4e4]"
+        className="flex text-lg items-center justify-center border-solid border-[2px] border-gray-200 w-full gap-2 pr-4 rounded-md text-gray-700 hover:bg-gray-200 max-w-[20em] font-bold"
         href={loginURL}
       >
-        <div className="max-w-[60px]" ref={newAnimation}></div>
-        Login with google
+        <div className="max-w-[50px]" ref={googleAnimationContainer}></div>
+        Login with Google
       </a>
     </div>
   );
---
+++ src/helpers/useNonce.js
@@ -0,0 +1,76 @@
+import { generateNonce } from "@mysten/zklogin";
+import { SuiClient, getFullnodeUrl } from "@mysten/sui.js/client";
+import { Ed25519Keypair } from "@mysten/sui.js/keypairs/ed25519";
+import { useEffect, useState } from "react";
+// var Buffer = require("buffer/").Buffer;
+import { Buffer } from "buffer";
+
+function generateRandomBytes(length) {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  let result = "";
+
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+}
+
+function generateRandomness() {
+  const randomBytesArray = generateRandomBytes(16); // Generate 16 random bytes
+  window.global = window;
+  window.Buffer = window.Buffer || require("buffer").Buffer;
+  const bigIntValue = Buffer.from(randomBytesArray).reduce(
+    // eslint-disable-next-line no-undef
+    (acc, byte) => (acc << 8n) | BigInt(byte),
+    0n
+  );
+  return bigIntValue;
+}
+
+const getActiveNetworkSuiClient = async () => {
+  try {
+    // Create a Sui Client
+    const client = new SuiClient({ url: getFullnodeUrl("testnet") });
+
+    // Optionally, you can initialize the client or perform other configurations here
+
+    // Return the initialized client
+    return client;
+  } catch (error) {
+    console.error("Error creating Sui Client:", error);
+    throw error; // Handle the error
+  }
+};
+
+export const useNonce = () => {
+  const [nonce, setNonce] = useState(null);
+
+  useEffect(() => {
+    const getNonce = async () => {
+      try {
+        const suiClient = await getActiveNetworkSuiClient();
+        const { epoch } = await suiClient.getLatestSuiSystemState();
+
+        const maxEpoch = epoch + 2; // this means the ephemeral key will be active for 2 epochs from now.
+        const ephemeralKeyPair = new Ed25519Keypair();
+        const randomness = generateRandomness();
+        const calculatedNonce = generateNonce(
+          ephemeralKeyPair.getPublicKey(),
+          maxEpoch,
+          randomness
+        );
+        console.log(calculatedNonce);
+
+        setNonce(calculatedNonce);
+      } catch (error) {
+        console.error("Error getting nonce:", error);
+      }
+    };
+    getNonce();
+  }, []);
+
+  return { nonce };
+};
