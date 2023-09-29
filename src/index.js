bc2c623b4f729ebc68c5e68649af26c9ee5a5a94
dolorunsesan_ <98563443+dolorunsesan@users.noreply.github.com>
2023-09-29 15:38:07 +0100

    Revert "chore: clean url and nav to new"

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import * as ReactDOM from "react-dom/client";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import Dashboard from "./dashboard";
@@ -11,14 +11,14 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
   <Layout>
-    <HashRouter>
+    <BrowserRouter>
       <AuthProvider>
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </AuthProvider>
-    </HashRouter>
+    </BrowserRouter>
   </Layout>
   // </React.StrictMode>
 );
